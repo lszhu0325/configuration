087d60b21236157e6bada376e079859e807e7369
lszhu0325 <lszhu0325@126.com>
2020-04-08 18:53:21 +0800

    log4j2日志配置提交

--- spring-config/src/main/java/com/lszhu0325/springconfig/log4j2/Log4j2Controller.java
+++ spring-config/src/main/java/com/lszhu0325/springconfig/log4j2/Log4j2Controller.java
@@ -1,5 +1,7 @@
 package com.lszhu0325.springconfig.log4j2;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,8 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/log4j2Controller")
 public class Log4j2Controller {
 
+    private static final Logger logger = LoggerFactory.getLogger(Log4j2Controller.class);
+
     @RequestMapping(value = "/helloWorld", method = RequestMethod.POST)
     public String helloWorld() {
+        logger.error("测试log4j2日志配置是否成功");
         return "welcome to SpringBoot";
     }
 }
