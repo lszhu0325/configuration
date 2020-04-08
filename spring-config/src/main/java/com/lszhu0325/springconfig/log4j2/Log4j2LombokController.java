11e79198b42306f090ef0758c211c56adb621c6b
lszhu0325 <lszhu0325@126.com>
2020-04-08 20:04:21 +0800

    log4j2和lombok结合使用

--- spring-config/src/main/java/com/lszhu0325/springconfig/log4j2/Log4j2LombokController.java
+++ spring-config/src/main/java/com/lszhu0325/springconfig/log4j2/Log4j2LombokController.java
@@ -1,7 +1,6 @@
 package com.lszhu0325.springconfig.log4j2;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,15 +8,14 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * Create by lenovo on 2020/4/8
  */
+@Slf4j
 @RestController
-@RequestMapping(value = "/log4j2Controller")
-public class Log4j2Controller {
-
-    private static final Logger logger = LoggerFactory.getLogger(Log4j2Controller.class);
+@RequestMapping(value = "/log4j2LombokController")
+public class Log4j2LombokController {
 
     @RequestMapping(value = "/helloWorld", method = RequestMethod.POST)
     public String helloWorld() {
-        logger.error("测试log4j2日志配置是否成功");
-        return "welcome to Log4j2";
+        log.error("log4j2 with lombok 的测试");
+        return "welcome to Log4j2 with lombok";
     }
 }
