7d60fd467d3e2552b9517db6f5b5b35d4ab5908a
lszhu0325 <lszhu0325@126.com>
2020-04-10 22:19:56 +0800

    springcloud-eureka注册中心配置

---
+++ spring-cloud/spring-cloud-eureka-1/src/test/java/com/lszhu0325/springcloudeureka1/SpringCloudEureka1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lszhu0325.springcloudeureka1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudEureka1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ spring-cloud/spring-cloud-eureka-2/src/test/java/com/lszhu0325/springcloudeureka2/SpringCloudEureka2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lszhu0325.springcloudeureka2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudEureka2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
